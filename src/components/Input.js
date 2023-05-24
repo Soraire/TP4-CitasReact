26cbcf1f64a8d8555508f7257d7b3290c8ddd61b
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-05-24 12:08:18 -0300

    24/5 final

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,7 @@
 export default function Input({ type, name, placeholder = "", title }) {
-return (<>
+return (
+<>
 <label>{title}</label>
 <input type={type} name={name} className="u-full-width" placeholder={placeholder}/>
-</>)
-}
+</>
+)}
